722330d6b3801d6ea16d57f2b0476f12bc2deb2f
docfr10 <docfr10@yandex.ru>
2023-12-04 18:42:17 +0300

    Added 2 Fragments and ListView

---
+++ app/src/main/java/com/example/lesson_fragments/BlankFragment.kt
@@ -0,0 +1,30 @@
+package com.example.lesson_fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.lesson_fragments.databinding.FragmentBlankBinding
+
+class BlankFragment : Fragment() {
+    private lateinit var binding: FragmentBlankBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBlankBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView: TextView = view.findViewById(R.id.textView)
+        val button: Button = view.findViewById(R.id.button)
+    }
+}
--- app/src/main/java/com/example/lesson_fragments/MainActivity.kt
+++ app/src/main/java/com/example/lesson_fragments/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.lesson_fragments
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, BlankFragment()).commit()
+
+        val list = listOf(
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C"
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        val listView: ListView = findViewById(R.id.listView)
+
+        listView.adapter = adapter
     }
 }
