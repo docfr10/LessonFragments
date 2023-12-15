603ec3c2def75336bde0a3f7a65dfcf54e4da69d
docfr10 <docfr10@yandex.ru>
2023-12-15 14:14:03 +0300

    Added sharedPrefs, intents, AlertDialog

--- app/src/main/java/com/example/lesson_fragments/BlankFragment.kt
+++ app/src/main/java/com/example/lesson_fragments/BlankFragment.kt
@@ -1,11 +1,14 @@
 package com.example.lesson_fragments
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.lesson_fragments.databinding.FragmentBlankBinding
 
@@ -26,5 +29,56 @@ class BlankFragment : Fragment() {
 
         val textView: TextView = view.findViewById(R.id.textView)
         val button: Button = view.findViewById(R.id.button)
+
+        val sharedPrefs = this.activity?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        sharedPrefs?.edit()?.putString("key", "value")?.apply()
+        textView.text = sharedPrefs?.getString("key", "")
+
+        button.setOnClickListener {
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.constraint, BlankFragment()).commit()
+
+//            sharedPrefs?.edit()?.putString("key", "123456787543234567")?.apply()
+//            textView.text = sharedPrefs?.getString("key", "")
+
+//            sharedPrefs?.edit()?.remove("key")?.apply()
+//            sharedPrefs?.edit()?.clear()?.apply()
+
+//            val url = Uri.parse("https://www.google.com")
+//            val intent = Intent(Intent.ACTION_VIEW, url)
+//            startActivity(intent)
+
+//            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+//            startActivity(intent)
+
+//            val intent = Intent(Intent.ACTION_SEND)
+//            intent.type = "text/plain"
+//            intent.putExtra(Intent.EXTRA_TEXT, "Some message")
+//            startActivity(Intent.createChooser(intent, "Выберите приложение"))
+
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(activity)
+
+        builder.setTitle("Title")
+        builder.setMessage("Message")
+
+        builder.setPositiveButton("Yes") { _, _ ->
+            Toast.makeText(activity, "YES", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.setNegativeButton("No") { _, _ ->
+            Toast.makeText(activity, "NO", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.setNeutralButton("Cancel") { _, _ ->
+            Toast.makeText(activity, "CANCEL", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.create().show()
     }
 }
