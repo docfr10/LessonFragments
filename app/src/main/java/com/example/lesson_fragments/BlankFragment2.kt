fbb59a0ff6cf5b112b127cf17c0845489d4da20d
docfr10 <docfr10@yandex.ru>
2024-12-10 09:51:50 +0300

    Updated code

--- app/src/main/java/com/example/lesson_fragments/BlankFragment2.kt
+++ app/src/main/java/com/example/lesson_fragments/BlankFragment2.kt
@@ -1,6 +1,8 @@
 package com.example.lesson_fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,39 +10,30 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("FRAGMENT 2", "onAttach")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        Log.i("FRAGMENT 2", "onCreate")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("FRAGMENT 2", "onCreateView")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Log.i("FRAGMENT 2", "onViewCreated")
 
         view.findViewById<TextView>(R.id.textView3)?.setOnClickListener {
             // Заменяем текущий фрагмент на новый
@@ -52,26 +45,35 @@ class BlankFragment2 : Fragment() {
             fragmentTransaction.addToBackStack(null)  // Добавляем в стек, чтобы можно было вернуться
             fragmentTransaction.commit()
         }
-
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        Log.i("FRAGMENT 2", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("FRAGMENT 2", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("FRAGMENT 2", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("FRAGMENT 2", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("FRAGMENT 2", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("FRAGMENT 2", "onDestroy")
     }
 }
